2943ccf0ac841cdb7ce41599939f9180f1a19d04
Unknown <punn.nara@gmail.com>
2017-10-15 08:05:42 +0700

    add test lint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { Routes } from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { ClientComponent } from './planner/client/client.component';
-import { CreateClientComponent } from './planner/client/create-client/create-client.component';
-import { DepotComponent } from './planner/depot/depot.component';
-import { CreateDepotComponent } from './planner/depot/create-depot/create-depot.component';
-import { PlannerComponent } from './planner/planner.component';
-import { PlannerHistoryComponent } from './planner/planner-history/planner-history.component';
-import { PlanningComponent } from './planner/planning/planning.component';
-import { PlanningResultComponent } from './planner/planning-result/planning-result.component';
-import { DriverComponent } from './driver/driver.component';
-import { DriverHomeComponent } from './driver/driver-home/driver-home.component';
-import { TodoDriverComponent } from './driver/todo-driver/todo-driver.component';
-import { DrivingResultComponent } from './driver/driving-result/driving-result.component';
-import { ErrorPageComponent } from './error-page/error-page.component';
-import { HeaderComponent } from './header/header.component';
-import { AppRoutingModule } from './app-routing.module';
-import { AuthService } from './auth.service';
-import { AuthGuard } from './auth-guard.service';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+import { DriverHomeComponent } from "./driver/driver-home/driver-home.component";
+import { DriverComponent } from "./driver/driver.component";
+import { DrivingResultComponent } from "./driver/driving-result/driving-result.component";
+import { TodoDriverComponent } from "./driver/todo-driver/todo-driver.component";
+import { ErrorPageComponent } from "./error-page/error-page.component";
+import { HeaderComponent } from "./header/header.component";
+import { LoginComponent } from "./login/login.component";
+import { ClientComponent } from "./planner/client/client.component";
+import { CreateClientComponent } from "./planner/client/create-client/create-client.component";
+import { CreateDepotComponent } from "./planner/depot/create-depot/create-depot.component";
+import { DepotComponent } from "./planner/depot/depot.component";
+import { PlannerHistoryComponent } from "./planner/planner-history/planner-history.component";
+import { PlannerComponent } from "./planner/planner.component";
+import { PlanningResultComponent } from "./planner/planning-result/planning-result.component";
+import { PlanningComponent } from "./planner/planning/planning.component";
+
+import { AuthGuard } from "./auth-guard.service";
+import { AuthService } from "./auth.service";
 
 @NgModule({
+    bootstrap: [AppComponent],
   declarations: [
     AppComponent,
     LoginComponent,
@@ -39,13 +42,12 @@ import { AuthGuard } from './auth-guard.service';
     ErrorPageComponent,
     HeaderComponent,
     PlannerHistoryComponent,
-    DriverHomeComponent
+    DriverHomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [AuthGuard, AuthService],
-  bootstrap: [AppComponent]
 })
 export class AppModule { }
