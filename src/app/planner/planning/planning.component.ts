fdb8d0433d64bd9f45dd6baa928080173f9587fe
Unknown <punn.nara@gmail.com>
2017-10-23 13:10:11 +0700

    add planning form phase 2

--- src/app/planner/planning/planning.component.ts
+++ src/app/planner/planning/planning.component.ts
@@ -1,20 +1,34 @@
+import { Subject } from 'rxjs/Subject';
+import { Subscription } from 'rxjs/Subscription';
 import { DriverService } from './../../services/driver.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { DatatableComponent } from '@swimlane/ngx-datatable';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-planning',
   templateUrl: './planning.component.html',
-  styleUrls: ['./planning.component.css']
+  styleUrls: ['./planning.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class PlanningComponent implements OnInit {
+export class PlanningComponent implements OnInit, OnDestroy {
+  @ViewChild(DatatableComponent) driverTable: DatatableComponent;
   lat: number = 51.678418;
   lng: number = 7.809007;
   numOfDrivers = 2;
+  numOfDriversSubject = new Subject<number>();
+  numOfDriversSubScription: Subscription;
+
+  selectedDriver = [];
+  numOfSelectedDriverSubject = new Subject<number>();
+  numOfSelectedDriverSubScription: Subscription;
+
   drivers = [];
+  tempDrivers = [];
 
   planningInfoGroup: FormGroup;
-  DriverFormGroup: FormGroup;
+  driverFormGroup: FormGroup;
   secondFormGroup: FormGroup;
 
   constructor(
@@ -27,24 +41,68 @@ export class PlanningComponent implements OnInit {
       response.drivers.forEach((driver) => {
         this.drivers.push(driver);
       });
+      this.tempDrivers = [...this.drivers];
     });
 
+    // Planning
     this.planningInfoGroup = this.formBuilder.group({
       date: new FormControl(null, Validators.required),
       hour: new FormControl(null, Validators.required),
       minute: new FormControl(null, Validators.required),
       method: new FormControl('distance'),
     });
-    this.DriverFormGroup = this.formBuilder.group({
-      capacity: new FormControl(null, Validators.required)
+
+    // Drivers
+    this.driverFormGroup = this.formBuilder.group({
+      capacity: new FormControl(null, [Validators.required]),
+      driverTable: new FormControl(null, this.checkDriverSelected.bind(this))
     });
+
+    this.numOfDriversSubScription = this.numOfDriversSubject.subscribe((value) => {
+      this.driverFormGroup.get('driverTable').setValidators(this.checkDriverSelected.bind(this));
+      this.driverFormGroup.get('driverTable').updateValueAndValidity();
+    });
+
+    this.numOfSelectedDriverSubScription = this.numOfSelectedDriverSubject.subscribe((value) => {
+      this.driverFormGroup.get('driverTable').setValidators(this.checkDriverSelected.bind(this));
+      this.driverFormGroup.get('driverTable').updateValueAndValidity();
+    });
+
+    // Clients
     this.secondFormGroup = this.formBuilder.group({
       secondCtrl: ['', Validators.required]
     });
   }
 
-  onSliderChange(e) {
+  updateFilter(e) {
+    const val = e.target.value.toLowerCase();
+
+    const temp = this.tempDrivers.filter((data) => {
+      return data.name.toLowerCase().indexOf(val) !== -1 || !val;
+    });
+
+    this.drivers = temp;
+    this.driverTable.offset = 0;
+  }
+
+  onSelect({ selected }) {
+    const numOfSelected = !this.selectedDriver ? 0 : this.selectedDriver.length;
+    this.numOfSelectedDriverSubject.next(numOfSelected);
+    this.selectedDriver.splice(0, this.selectedDriver.length);
+    this.selectedDriver.push(...selected);
+  }
+
+  checkDriverSelected(control: FormControl): {[s: string]: boolean} {
+    if (this.numOfDrivers !== this.selectedDriver.length) {
+      return({selectedDriverError: true});
+    } else {
+      return(null);
+    }
+  }
+
+  onDriverSliderChange(e) {
     this.numOfDrivers = e.value;
+    this.numOfDriversSubject.next(e.value);
   }
 
   onHourBlur(e) {
@@ -65,20 +123,13 @@ export class PlanningComponent implements OnInit {
     return date >= currentDate;
   }
 
-  getErrorMessage() {
-    if (this.planningInfoGroup.get('date').hasError('matDatepickerFilter')) {
-      return 'You must select correct date';
-    } else if (
-        this.planningInfoGroup.get('date').hasError('required') ||
-        this.planningInfoGroup.get('minute').hasError('required') ||
-        this.planningInfoGroup.get('method').hasError('required')) {
-      return 'You must enter a value';
-    }
-    return '';
+  test() {
+    
   }
 
-  test() {
-    console.log(this.planningInfoGroup.value);
+  ngOnDestroy() {
+    this.numOfDriversSubScription.unsubscribe();
+    this.numOfSelectedDriverSubScription.unsubscribe();
   }
 
 }
