2b6d38c73cba413cd6e428d58d9febcc23a73ed1
Unknown <punn.nara@gmail.com>
2017-11-19 18:32:54 +0700

    add planning page 3

--- src/app/planner/planning/planning.component.ts
+++ src/app/planner/planning/planning.component.ts
@@ -1,11 +1,16 @@
+import { Component, OnInit, ViewChild, OnDestroy, ViewEncapsulation, NgZone, ElementRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+
 import { DepotService } from './../../services/depot.service';
+import { DriverService } from './../../services/driver.service';
+
 import { Subject } from 'rxjs/Subject';
 import { Subscription } from 'rxjs/Subscription';
-import { DriverService } from './../../services/driver.service';
-import { Component, OnInit, ViewChild, OnDestroy, ViewEncapsulation } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { DatatableComponent } from '@swimlane/ngx-datatable';
 import { Observable } from 'rxjs/Observable';
+
+import { MapsAPILoader } from '@agm/core';
+import { } from 'googlemaps';
 
 import { Marker } from '../../shared/marker';
 import { Map } from '../../shared/map';
@@ -17,19 +22,29 @@ import { Map } from '../../shared/map';
   encapsulation: ViewEncapsulation.None
 })
 export class PlanningComponent implements OnInit, OnDestroy {
+  @ViewChild(DatatableComponent) depotTable: DatatableComponent; 
+  @ViewChild(DatatableComponent) clientTable: DatatableComponent; 
+  @ViewChild("searchMap") public searchElementRef: ElementRef;
+
   map = new Map;
+  searchLocationInput;
+  offset = 0;
   
   depots = [];
+  tempDepots = [];
   selectedDepot = [];
   depotMarker: Marker;
   numOfSelectedDepotSubject = new Subject<number>();
   numOfSelectedDepotSubScription: Subscription;
   
   clients = [];
+  tempClients = [];
   selectedClient = [];
   clientMarker: Marker;
-  numOfSelectedClienttSubject = new Subject<number>();
+  numOfSelectedClientSubject = new Subject<number>();
   numOfSelectedClientSubScription: Subscription;
+
+  searchMarker: Marker;
 
   planningInfoGroup: FormGroup;
   depotGroup: FormGroup;
@@ -38,12 +53,41 @@ export class PlanningComponent implements OnInit, OnDestroy {
   constructor(
     private formBuilder: FormBuilder,
     private driverService: DriverService,
-    private depotService: DepotService
+    private depotService: DepotService,
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone,
   ) { }
 
   ngOnInit() {
+    this.mapsAPILoader.load().then(() => {
+      const autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement);
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          // get the place result
+          const place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          // verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          // set latitude, longitude and zoom
+          this.map.zoom = 18;
+          this.map.lat = place.geometry.location.lat();
+          this.map.lng = place.geometry.location.lng();
+
+          this.searchMarker = {
+            lat: this.map.lat,
+            lng: this.map.lng,
+            draggable: false
+          };
+        });
+      });
+    });
+
     this.depotService.getAllDepots().then((response) => {
       this.depots = response;
+      this.tempDepots = this.depots;
     });
 
     // Planning
@@ -52,7 +96,7 @@ export class PlanningComponent implements OnInit, OnDestroy {
       time: new FormControl(null, Validators.required),
       numOfDrivers: new FormControl(null, Validators.required),
       capacity: new FormControl(null, Validators.required),
-      method: new FormControl('distance'),
+      method: new FormControl('distance')
     });
 
     // Depot
@@ -68,6 +112,11 @@ export class PlanningComponent implements OnInit, OnDestroy {
     this.numOfSelectedDepotSubScription = this.numOfSelectedDepotSubject.subscribe((value) => {
       this.depotGroup.get('depotTable').setValidators(this.checkDepotSelected.bind(this));
       this.depotGroup.get('depotTable').updateValueAndValidity();
+    });
+
+    this.numOfSelectedClientSubScription = this.numOfSelectedClientSubject.subscribe((value) => {
+      this.clientGroup.get('clientTable').setValidators(this.checkClientSelected.bind(this));
+      this.clientGroup.get('clientTable').updateValueAndValidity();
     });
   }
 
@@ -87,6 +136,28 @@ export class PlanningComponent implements OnInit, OnDestroy {
     }
   }
 
+  updateDepotFilter(e) {
+    const val = e.target.value.toLowerCase();
+
+    const temp = this.tempDepots.filter((data) => { 
+      return data.depotName.toLowerCase().indexOf(val) !== -1 || !val; 
+    }); 
+
+    this.depots = temp; 
+    this.depotTable.offset = 0; 
+  }
+
+  updateClientFilter(e) {
+    const val = e.target.value.toLowerCase();
+
+    const temp = this.tempDepots.filter((data) => { 
+      return data.depotName.toLowerCase().indexOf(val) !== -1 || !val; 
+    }); 
+
+    this.clients = temp; 
+    this.clientTable.offset = 0; 
+  }
+
   dateFilter(date: Date): boolean {
     const currentDate = new Date();
     currentDate.setDate(currentDate.getDate() - 1);
@@ -104,6 +175,16 @@ export class PlanningComponent implements OnInit, OnDestroy {
     this.map.zoom = 15;
     this.numOfSelectedDepotSubject.next(selected.length);
   }
+
+  addMockClient() {
+    this.offset += 0.01
+    this.clients.push({
+      clientName: 'test Name',
+      telephoneNumber: '1231564',
+      address: 'test address',
+      coordinate: [13.6526 + this.offset, 100.486328 + this.offset]
+    })
+  }
   
   test() {
 
@@ -111,8 +192,7 @@ export class PlanningComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.numOfSelectedDepotSubScription.unsubscribe();
-    // this.numOfDriversSubScription.unsubscribe();
-    // this.numOfSelectedDriverSubScription.unsubscribe();
+    this.numOfSelectedClientSubScription.unsubscribe();
   }
 
 }
